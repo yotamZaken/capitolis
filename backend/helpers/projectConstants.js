4003b71700289227af1efc324f8ff653b487822a
yotamZaken <yotamzk@gmail.com>
2021-06-15 16:09:38 +0300

    Moving to env vars for DB url to protect password

--- backend/helpers/projectConstants.js
+++ backend/helpers/projectConstants.js
@@ -1,4 +1,4 @@
 module.exports = {
-    DB_URI: 'mongodb+srv://capitolis-app:M1TNiEhIOC9OVWwS@us-east-1-yz.0fwoo.mongodb.net/capitolis?retryWrites=true&w=majority',
+    DB_URI: process.env.DB_URI,
     SERVER_PORT: 3001
 }
